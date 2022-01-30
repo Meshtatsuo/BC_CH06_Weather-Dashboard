e7e6988f71048c06a99b8bd0a99b7e453d223d2c
Dominic Misasi <meshtatsuo@gmail.com>
2022-01-29 20:12:30 -0700

    Skeletal project setup

---
+++ assets/js/guiManager.js
@@ -0,0 +1,4 @@
+// GUI MANAGER
+// This script manages the displaying of elements
+// on the page. Keeping the OpenWeather API requests
+// seperate from the updating and displaying of elements
---
+++ assets/js/weatherQuery.js
@@ -0,0 +1,4 @@
+// WEATHER QUERY SCRIPT
+// This script handles the fetching,
+// receiving, validating, and parsing
+// data from the OpenWeather API
